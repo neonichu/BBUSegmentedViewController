1e711a14cb8f48314c684b827e127f7b5297a3cb
Boris Bügling <boris@icculus.org>
2014-05-29 06:26:39 -0700

    Documentation!

--- BBUSegmentedViewController.h
+++ BBUSegmentedViewController.h
@@ -8,15 +8,40 @@
 
 #import <UIKit/UIKit.h>
 
+/**
+ *  Transition style used when the user changes the selected index by tapping.
+ */
 typedef NS_ENUM(NSInteger, BBUSegmentedViewTransitionStyle) {
+    /**
+     *  As seen in UIScrollView
+     */
     BBUSegmentedViewTransitionStyleScrolling,
+    /**
+     *  Slide from left-to-right.
+     */
     BBUSegmentedViewTransitionStyleLeftToRight,
+    /**
+     *  Slide from right-to-left.
+     */
     BBUSegmentedViewTransitionStyleRightToLeft,
+    /**
+     *  Hard switch, no animation.
+     */
     BBUSegmentedViewTransitionStyleNone,
 };
 
+/**
+ *  The segmented view controller provides a simple way of presenting a way to
+ *  switch between multiple view controllers using a segmented control.
+ *
+ *  It simply works by displaying the child view controllers of this view
+ *  controller.
+ */
 @interface BBUSegmentedViewController : UIViewController
 
+/**
+ *  Transition style to use when switching between child view controllers.
+ */
 @property (nonatomic) BBUSegmentedViewTransitionStyle transitionStyle;
 
 @end
